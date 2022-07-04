e7513b1f994c458a1e7a4a300673c07b518e6820
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-04 13:22:31 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,49 +1,49 @@
 //1.
-let parkWalkStr = "I can walk in the park all day!";
+const parkWalkStr = "I can walk in the park all day!";
 console.log(parkWalkStr.slice(-13,-8));
 
 //2.
-let helloStr =  "Hello World";
+const helloStr =  "Hello World";
 console.log(helloStr.toUpperCase());
 
 //3.
-let earthlingsStr =  "Hello Earthling";
+const earthlingsStr =  "Hello Earthling";
 console.log(earthlingsStr.toLowerCase());
 
 //4.
-let jsStr = "JavaScript";
+const jsStr = "JavaScript";
 console.log(jsStr.substring(3,6));
 
 //5.
-let niceShoesStr = "nice shoes";
+const niceShoesStr = "nice shoes";
 console.log(niceShoesStr.includes('l')); 
 console.log(niceShoesStr.includes('n'));
 
 //6.
-let oldStr = 'choice';
-let newStr = oldStr.substring(0,1) + oldStr + oldStr.substring(0,1);
+const oldStr = 'choice';
+const newStr = oldStr.substring(0,1) + oldStr + oldStr.substring(0,1);
 console.log(newStr);
 
 //7.
-let scritcherStr = 'Scritch'; 
-let lastThreeChars = scritcherStr.slice(-3);
+const scritcherStr = 'Scritch'; 
+const lastThreeChars = scritcherStr.slice(-3);
 console.log(lastThreeChars + scritcherStr + lastThreeChars);
 
 //8.
 let boogieNightsStr = 'BoogieWoogie';
-let firstChar = boogieNightsStr[0];
+const firstChar = boogieNightsStr[0];
 boogieNightsStr = boogieNightsStr.replace(boogieNightsStr[0], boogieNightsStr.slice(-1));
 boogieNightsStr = boogieNightsStr.slice(0,-1)+ firstChar;
 console.log(boogieNightsStr);
 
 
 //9.
-let firstName = 'Rami';
-let city = 'The Valley of The Wind';
-let job = 'Software Engineering prince of Narnia XD';
+const firstName = 'Rami';
+const city = 'The Valley of The Wind';
+const job = 'Software Engineering prince of Narnia XD';
 console.log('My name is', firstName + '. I live in '+city+' and I am the '+ job +'.');
 
 //10.
-let foxStr = 'the quick brown fox';
-let capitalizeFirst = foxStr[0].toUpperCase() + foxStr.substring(1);
+const foxStr = 'the quick brown fox';
+const capitalizeFirst = foxStr[0].toUpperCase() + foxStr.substring(1);
 console.log(capitalizeFirst);
